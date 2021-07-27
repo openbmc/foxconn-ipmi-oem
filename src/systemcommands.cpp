4b9e6bec63e0922067385c8fadbafd0ce0f68077
Ashwin Murali <ashwin.murali@fii-na.com>
2021-07-27 11:00:03 -0500

    foxconn-ipmi-oem: Modify PCIe bifurcation command internals
    Modify PCIe bifurcation and PCIe count to use fii-cpld driver instead
    of reading using i2cget directly
    
    Signed-off-by: Ashwin Murali <ashwin.murali@fii-na.com>
    Change-Id: Ic2745e988e8cb87a6c93d2428b0fe8a78e98ba67

--- src/systemcommands.cpp
+++ src/systemcommands.cpp
@@ -1,7 +1,7 @@
 /********************************************************************************
 *                       HON HAI Precision IND.Co., LTD.                         *
 *            Personal Computer & Enterprise Product Business Group              *
-*                      Enterprise Product Business Gro:qup                      *
+*                      Enterprise Product Business Group                        *
 *                                                                               *
 *     Copyright (c) 2010 by FOXCONN/CESBG/CABG/SRD. All rights reserved.        *
 *     All data and information contained in this document is confidential       *
@@ -19,53 +19,65 @@
 #include <ipmid/api.hpp>
 #include <phosphor-logging/log.hpp>
 #include <sdbusplus/message/types.hpp>
+#include <fstream>
 
 namespace ipmi
-    {
-    static void registerSystemFunctions() __attribute__((constructor));
+{
+static void registerSystemFunctions() __attribute__((constructor));
 
-    ipmi::RspType<std::vector<uint8_t>> FiiSysPCIeInfo(boost::asio::yield_context yield)
-    {
-        std::vector<uint8_t> rsp;
-        char buffer[128], *token;
-        uint32_t value;
-        FILE *pipe = popen(PCIEINFO_COMMAND, "r");
+ipmi::RspType<uint8_t, uint8_t> FiiSysPCIeInfo(boost::asio::yield_context yield)
+{
+    uint8_t bifurcation, presence;
 
-        // Read pcie bifurcation information
-        // it return two bytes, 1st byte bifurcation, 2nd byte present pin
-        if (!pipe) throw std::runtime_error("popen() failed !!!");
-        while (fgets(buffer, sizeof(buffer), pipe) != NULL)
-        {
-            std::cerr << " Command : " << buffer << std::endl;
-        }
-        pclose(pipe);
+    std::ifstream infile;
+    try {
+        infile.open(PCIEINFO_REG);
+    }
+    catch (const std::ifstream::failure& e) {
+        std::cerr << "Error opening/reading file" << std::endl;
+    }
+    std::string str;
 
-        token = std::strtok(buffer, " ");
-        if (token == NULL)
-        {
-            phosphor::logging::log<phosphor::logging::level::ERR>(
-                "Fii system cmd : Error geting PCIe Info came back null");
-            ipmi::responseUnspecifiedError();
-        }
-        token = std::strtok(NULL, " ");
-        while (token != NULL)
-        {
-            //std::cerr << " Command token: " << token << std::endl;
-            value = std::stoul(token, nullptr, 16);
-            //std::cerr << " Command value: " << value << ":" << std::hex << value << std::endl;
-            rsp.push_back(static_cast<uint8_t>(value & 0xFF));
-            token = std::strtok(NULL, " ");
-        }
+    std::getline(infile, str);
 
-        return ipmi::responseSuccess(rsp);
+    infile.close();
+
+    if (str.empty()) {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Fii system cmd : Error geting PCIe Info came back null");
+        ipmi::responseUnspecifiedError();
     }
 
-    void registerSystemFunctions()
-    {
-        std::fprintf(stderr, "Registering OEM:[0x34], Cmd:[%#04X] for Fii System OEM Commands\n", FII_CMD_SYS_PCIE_INFO);
-        ipmi::registerHandler(ipmi::prioOemBase, ipmi::netFnOemThree, FII_CMD_SYS_PCIE_INFO, ipmi::Privilege::User,
-                FiiSysPCIeInfo);
+    std::sscanf(str.c_str(), "%x %x", &bifurcation, &presence);
 
-        return;
-    }
+    return ipmi::responseSuccess(bifurcation, presence);
 }
+
+ipmi::RspType<uint8_t, uint8_t, uint8_t, uint8_t> ipmiAppGetSystemIfCapabilities(uint8_t iface)
+{
+
+    // Per IPMI 2.0 spec, the input and output buffer size must be the max
+    // buffer size minus one byte to allocate space for the length byte.
+    constexpr uint8_t reserved = 0x00;
+    constexpr uint8_t support = 0b10000000;
+    constexpr uint8_t inMsgSize = 240;
+    constexpr uint8_t outMsgSize = 240;
+
+    return ipmi::responseSuccess(reserved, support,
+                                inMsgSize, outMsgSize);
+}
+
+void registerSystemFunctions()
+{
+    std::fprintf(stderr, "Registering OEM:[0x34], Cmd:[%#04X] for Fii System OEM Commands\n",
+        FII_CMD_SYS_PCIE_INFO);
+    ipmi::registerHandler(ipmi::prioOemBase, ipmi::netFnOemThree, FII_CMD_SYS_PCIE_INFO,
+        ipmi::Privilege::User, FiiSysPCIeInfo);
+
+    std::fprintf(stderr, "Registering APP:[0x06], Cmd:[%#04X] for Fii System OEM Commands\n",
+        ipmi::app::cmdGetSystemIfCapabilities);
+    ipmi::registerHandler(ipmi::prioOemBase, ipmi::netFnApp, ipmi::app::cmdGetSystemIfCapabilities,
+        ipmi::Privilege::User, ipmiAppGetSystemIfCapabilities);
+
+}
+} // namespace ipmi
