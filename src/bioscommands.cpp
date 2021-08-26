c554278569edb6ba72b53954bf1323a0c03bff6a
Avenash Asai Thambi <avenash.thambi@fii-usa.com>
2021-08-26 15:51:47 -0500

    foxconn-ipmi-oem: Add the boot count into eeprom instead of a file
    The bmc boot count which was previously stored into the file
    /etc/conf/bios.cnt, is now stored into the motherboard FRU eeprom
    /sys/bus/i2c/devices/4-0050/eeprom starting from offset of 1000.
    
    The reading and writing to the FRU eeprom is handled by using the helper
    functions provided by the fcntl.
    
    Modified the reading and writing of the bytes from vector of uint8_t to
    uint32_t which makes the code cleaner.
    
    Added the new IPMI clear command which sets the eeprom to the default
    value of ff ff ff ff - ipmitool raw 0x34 0x71 0x02.
    
    Use of the 5th byte from 1000 location to check if the boot counter is
    at the max value or at the default value.
    
    Cleaned unwanted variables, fixed the character limit and use of one function
    to write 4 bytes and 1 byte value instead of two.
    
    The above ipmi command functionality has been implemented using ipmitool raw
    0x34 0x71 and also with the extended oem ipmi command with foxconn's
    IANA number 17084 ipmitool raw 0x2e 0x32 0xbc 0x42 0x00 using ipmi_ret_t.
    Foxconn oem number is added into the phosphor-host-ipmid package.
    
    Tested:
    ipmitool raw 0x34 0x71 0x00 - read current boot count.
    ipmitool raw 0x34 0x71 0x01 - increment the boot count by 1.
    ipmitool raw 0x34 0x71 0x01 byte1 byte2 byte3 byte4 - set the
    boot count to desired value provided by the request parameter.
    ipmitool raw 0x34 0x71 0x02 - set the boot_count to all ff which is its
    default value of eeprom location where the boot count is stored.
    ipmitool raw 0x2e 0x32 0xbc 0x42 0x00 0x00 0x00 - read current boot count.
    ipmitool raw 0x2e 0x32 0xbc 0x42 0x00 0x00 0x01 - increment boot
    count by 1.
    ipmitool raw 0x2e 0x32 0xbc 0x42 0x00 0x00 0x01 byte1 byte2 byte3
    byte 4 - set the boot count to desired value provided by the request
    parameter.
    ipmitool raw 0x2e 0x32 0xbc 0x42 0x00 0x00 0x02 - set the boot count
    to all ff which is its default value of eeprom location where the boot
    count is stored.
    Reboot,powercyle,flashing bmc - performed to see if the value retains.
    
    Signed-off-by: Avenash Asai Thambi <avenash.thambi@fii-usa.com>
    Change-Id: I99dd5e6ba1d943e558e984d948bd63552cd5278f

--- src/bioscommands.cpp
+++ src/bioscommands.cpp
@@ -16,95 +16,401 @@
 
 #include <common.hpp>
 #include <bioscommands.hpp>
+#include <memory>
+#include <unistd.h>
+#include <boost/endian/arithmetic.hpp>
+#include <string.h>
+#include <fcntl.h>
+#include <ipmid/api.h>
+#include <cstdint>
+#include <cstdio>
+#include <functional>
+#include <ipmid/iana.hpp>
+#include <ipmid/oemrouter.hpp>
+
+using namespace std::string_literals;
+
+int fd_;
+size_t offset_;
+uint8_t op;
+int boot_check;
+uint32_t boot_count; 
+std::string eeprom_path = "/sys/bus/i2c/devices/4-0050/eeprom";
+
+/* Start of helper functions to read and write into eeprom /sys/bus/i2c/
+devices/4-0050/eeprom */
+
+std::system_error errnoException(const std::string& message)
+{
+    return std::system_error(errno, std::generic_category(), message);
+}
+
+void sysopen(const std::string& path, size_t offset)
+{
+    fd_ = open(path.c_str(), O_RDWR);
+    offset_ = offset;
+    if (fd_ < 0)
+    {
+        throw errnoException("Error opening file "s + path);
+    }
+}
+
+void lseek(size_t pos)
+{
+    if (lseek(fd_, offset_ + pos, SEEK_SET) < 0)
+    {
+        throw errnoException("Cannot lseek to pos "s + std::to_string(pos));
+    }
+}
+
+size_t readToBuf(size_t pos, size_t count, char* buf)
+{
+    lseek(pos);
+    size_t bytesRead = 0;
+    do
+    {
+        auto ret = read(fd_, &buf[bytesRead], count - bytesRead);
+        if (ret < 0)
+        {
+            if (errno == EINTR)
+            {
+                continue;
+            }
+            throw errnoException("Error reading from file"s);
+        }
+        else if (ret > 0)
+        {
+            bytesRead += ret;
+        }
+        else // ret == 0
+        {
+            break;
+        }
+    } while (bytesRead < count);
+    return bytesRead;
+}
+
+void writeBin(void *ptr, size_t pos, size_t size)
+{
+    lseek(pos);
+    ssize_t ret;
+    ret = write(fd_,ptr,size);
+    if (ret < 0)
+    {
+        throw errnoException("Error writing to file"s);
+    }
+    if (static_cast<size_t>(ret) != size)
+    {
+        throw std::runtime_error(
+                "Tried to send data size "s + std::to_string(size) +
+                " but could only send "s + std::to_string(ret));
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+/* End of helper functions to read and write into eeprom /sys/bus/i2c/
+devices/4-0050/eeprom */
+////////////////////////////////////////////////////////////////////////////
 
 namespace ipmi
 {
+
+/* Start of IPMI command which uses the Netfunction - 0x34 and command - 0x71 */
+
     static void registerBIOSFunctions() __attribute__((constructor));
 
-    ipmi::RspType<std::vector<uint8_t>> FiiBIOSBootCount(boost::asio::yield_context yield, std::vector<uint8_t> reqParams)
+    ipmi::RspType<uint32_t> FiiBIOSBootCount(boost::asio::yield_context yield, 
+            std::vector<uint8_t> reqParams)
     {
-        bool op;
-        std::vector<uint8_t> boot_count;
-        uint32_t counter = 0, ret;
-
         if (reqParams.empty())
         {
-            phosphor::logging::log<phosphor::logging::level::ERR>(" Fii bios cmd : command format error.");
-
+            phosphor::logging::log<phosphor::logging::level::ERR>(
+                    " Fii bios cmd : command format error.");
             return ipmi::responseReqDataLenInvalid();
         }
 
+        sysopen(eeprom_path,4096);
         op = reqParams[0] & 0b11;
-        // check the boot count file exist or not
-        std::fstream fptr(BOOT_COUNT_FILE);
+        boost::endian::little_uint8_t checker = 0;
+        readToBuf(4, sizeof(checker), reinterpret_cast<char*>(&checker));
+        boot_check = (int)checker;
 
-        if (!fptr.is_open())
+        if(op == OP_CODE_READ && boot_check == 255)
         {
-            std::cerr << " Fii bios cmd : file didn't exist and try to create one\n";
-            ret = system("mkdir -p /etc/conf");
-            std::ofstream outfile (BOOT_COUNT_FILE);
-            outfile << "0" << std::endl;
-            outfile.close();
-            boot_count.push_back(static_cast<uint8_t>(counter));
-            boot_count.push_back(static_cast<uint8_t>(counter >> 8));
-            boot_count.push_back(static_cast<uint8_t>(counter >> 16));
-            boot_count.push_back(static_cast<uint8_t>(counter >> 24));
-        }
-        else
-        {
-            std::string str;
-            while (std::getline(fptr, str))
-            {
-                //boot_count.push_back(static_cast<uint8_t>(std::stoul(str)));
-                counter = (std::stoul(str));
-                //std::cerr << " Fii bios cmd : " << counter << std::endl;
-            }
-            boot_count.push_back(static_cast<uint8_t>(counter));
-            boot_count.push_back(static_cast<uint8_t>(counter >> 8));
-            boot_count.push_back(static_cast<uint8_t>(counter >> 16));
-            boot_count.push_back(static_cast<uint8_t>(counter >> 24));
-            fptr.close();
-        }
-        if (op == OP_CODE_READ)
-        {
+            boot_count = 0x00000000;
             return ipmi::responseSuccess(boot_count);
         }
-        else if (op == OP_CODE_WRITE)
+
+        else if (op == OP_CODE_READ && boot_check == 1)
         {
-            uint32_t value = 0;
+            boost::endian::little_uint32_t read_buffer = 0;
+            readToBuf(0, sizeof(read_buffer), reinterpret_cast<char*>(&read_buffer));
+            return ipmi::responseSuccess(read_buffer);
+        }
+
+        else if (op == OP_CODE_CLEAR)
+        {
+            uint32_t default_val = 0xffffffff;
+            uint8_t clear_set = 0xff;
+            writeBin(&default_val,0,sizeof(default_val));
+            writeBin(&clear_set,4,sizeof(clear_set));
+            return ipmi::responseSuccess(default_val);
+
+        }
+
+        else if (op == OP_CODE_WRITE && boot_check == 255)
+        {
+            uint8_t boot_set = 0x01; 
             if (reqParams.size() == 1)
             {
-                value = boot_count[0] + (boot_count[1] << 8) + (boot_count[2] << 16) + (boot_count[3] << 24);
-                value += 1;
-                boot_count.clear();
-                boot_count.push_back(static_cast<uint8_t>(value));
-                boot_count.push_back(static_cast<uint8_t>(value >> 8));
-                boot_count.push_back(static_cast<uint8_t>(value >> 16));
-                boot_count.push_back(static_cast<uint8_t>(value >> 24));
+                boot_count = 0x01;
+                writeBin(&boot_count,0,sizeof(boot_count));
+                writeBin(&boot_set,4,sizeof(boot_set));
             }
+
             else if (reqParams.size() == FII_CMD_BIOS_BOOT_COUNT_LEN)
             {
-                value = reqParams[1] + + (reqParams[2] << 8) + (reqParams[3] << 16) + (reqParams[4] << 24);
-                boot_count.clear();
-                boot_count.insert(boot_count.begin(), reqParams.begin()+1, reqParams.end());
+                boot_count = reqParams[1] + + (reqParams[2] << 8) + (reqParams[3] << 16)
+                    + (reqParams[4] << 24);
+                writeBin(&boot_count,0,sizeof(boot_count));
+                writeBin(&boot_set,4,sizeof(boot_set));
             }
-            std::ofstream fptr_w(BOOT_COUNT_FILE, std::ios::out | std::ios::trunc);
-            fptr_w << value << std::endl;
-            fptr_w.close();
         }
+
+        else if (op == OP_CODE_WRITE && boot_check == 1)
+        {
+            boost::endian::little_uint32_t read_buffer = 0;
+            readToBuf(0, sizeof(read_buffer), reinterpret_cast<char*>(&read_buffer)); 
+            if (reqParams.size() == 1)
+            {
+                boot_count = read_buffer + 1;
+            }
+
+            else if (reqParams.size() == FII_CMD_BIOS_BOOT_COUNT_LEN)
+            {
+                boot_count = reqParams[1] + + (reqParams[2] << 8) + (reqParams[3] << 16)
+                    + (reqParams[4] << 24);
+            }
+            writeBin(&boot_count,0,sizeof(boot_count));
+        }
+
         else
         {
             return ipmi::responseInvalidCommand();
         }
+
         return ipmi::responseSuccess(boot_count);
     }
 
     void registerBIOSFunctions()
     {
-        std::fprintf(stderr, "Registering OEM:[0x34], Cmd:[%#04X] for Fii BIOS OEM Commands\n", FII_CMD_BIOS_BOOT_COUNT);
-        ipmi::registerHandler(ipmi::prioOemBase, ipmi::netFnOemThree, FII_CMD_BIOS_BOOT_COUNT, ipmi::Privilege::User,
-                FiiBIOSBootCount);
+        std::fprintf(stderr, "Registering OEM:[0x34], Cmd:[%#04X] for Fii BIOS OEM Commands\n",
+                FII_CMD_BIOS_BOOT_COUNT);
+        ipmi::registerHandler(ipmi::prioOemBase, ipmi::netFnOemThree, FII_CMD_BIOS_BOOT_COUNT,
+                ipmi::Privilege::User, FiiBIOSBootCount);
 
         return;
     }
+/////////////////////////////////////////////////////////////////////////////////////
+/*End of IPMI command which uses the Netfunction - 0x34 and command - 0x71 */
+/////////////////////////////////////////////////////////////////////////////////////
+
+/*Start of exteded IPMI OEM command which uses the oem, IANA number format */
+
+    struct biosrequest
+    {
+        uint8_t subcommand;
+        uint8_t op;
+    };
+
+    struct biossetrequest
+    {
+        uint8_t subcommand;
+        uint8_t op;
+        uint8_t set_byte1;
+        uint8_t set_byte2;
+        uint8_t set_byte3;
+        uint8_t set_byte4;
+    };
+
+    struct biosreply
+    {
+        uint8_t subcommand;
+        uint8_t count1;
+        uint8_t count2;
+        uint8_t count3;
+        uint8_t count4;
+    };
+
+    ipmi_ret_t FiiOemCount(const uint8_t* reqBuf, 
+            uint8_t* replyBuf, size_t* dataLen)
+    {
+        struct biosrequest request;
+        struct biossetrequest setrequest;
+        struct biosreply reply;
+        uint8_t boot_set;
+        sysopen(eeprom_path,4096);
+        boost::endian::little_uint8_t checker = 0;
+        readToBuf(4, sizeof(checker), reinterpret_cast<char*>(&checker));
+        boot_check = (int)checker;
+        
+        if ((*dataLen) < sizeof(request))
+        {
+            phosphor::logging::log<phosphor::logging::level::ERR>(
+                    " Fii bios cmd : command format error.");
+            return IPMI_CC_REQ_DATA_LEN_INVALID;
+        }
+        
+        if((*dataLen) == sizeof(request))
+        {
+            std::memcpy(&request, &reqBuf[0], sizeof(request));
+            if(request.op == 0 && boot_check == 1)
+            {
+                boost::endian::little_uint32_t read_buffer = 0;
+                readToBuf(0, sizeof(read_buffer), reinterpret_cast<char*>(&read_buffer));
+                reply.subcommand = 0x00;
+                reply.count1 = static_cast<uint8_t>(read_buffer);
+                reply.count2 = static_cast<uint8_t>(read_buffer >> 8);
+                reply.count3 = static_cast<uint8_t>(read_buffer >> 16);
+                reply.count4 = static_cast<uint8_t>(read_buffer >> 24);
+                std::memcpy(&replyBuf[0], &reply, sizeof(reply));
+                (*dataLen) = sizeof(reply);
+                return IPMI_CC_OK;
+            }
+            
+            else if(request.op == 0 && boot_check == 255)
+            {
+                boost::endian::little_uint32_t read_buffer = 0;
+                readToBuf(0, sizeof(read_buffer), reinterpret_cast<char*>(&read_buffer));
+                reply.subcommand = 0x00;
+                reply.count1 = 0x00;
+                reply.count2 = 0x00;
+                reply.count3 = 0x00;
+                reply.count4 = 0x00;
+                std::memcpy(&replyBuf[0], &reply, sizeof(reply));
+                (*dataLen) = sizeof(reply);
+                return IPMI_CC_OK;
+            }
+            
+            else if(request.op == 2)
+            {
+                boot_count = 0xffffffff;
+                boot_set = 0xff;
+                reply.subcommand = 0x00;
+                reply.count1 = 0xff;
+                reply.count2 = 0xff;
+                reply.count3 = 0xff;
+                reply.count4 = 0xff;
+                writeBin(&boot_count,0,sizeof(boot_count));
+                writeBin(&boot_set,4,sizeof(boot_count));
+                std::memcpy(&replyBuf[0], &reply, sizeof(reply));
+                (*dataLen) = sizeof(reply);
+                return IPMI_CC_OK;
+            }
+            
+            else if(request.op == 1 && boot_check == 1)
+            {
+                boost::endian::little_uint32_t read_buffer = 0;
+                readToBuf(0, sizeof(read_buffer), reinterpret_cast<char*>(&read_buffer));
+                boot_count = read_buffer + 1;
+                reply.subcommand = 0x00;
+                reply.count1 = static_cast<uint8_t>(boot_count);
+                reply.count2 = static_cast<uint8_t>(boot_count >> 8);
+                reply.count3 = static_cast<uint8_t>(boot_count >> 16);
+                reply.count4 = static_cast<uint8_t>(boot_count >> 24);
+                writeBin(&boot_count,0,sizeof(boot_count));
+                std::memcpy(&replyBuf[0], &reply, sizeof(reply));
+                (*dataLen) = sizeof(reply);
+                return IPMI_CC_OK;
+            }
+
+            else if(request.op == 1 && boot_check == 255)
+            {
+                boot_count = 0x00000001;
+                boot_set = 0x01;
+                reply.subcommand = 0x00;
+                reply.count1 = 0x01;
+                reply.count2 = 0x00;
+                reply.count3 = 0x00;
+                reply.count4 = 0x00;
+                writeBin(&boot_count,0,sizeof(boot_count));
+                writeBin(&boot_set,4,sizeof(boot_set));
+                std::memcpy(&replyBuf[0], &reply, sizeof(reply));
+                (*dataLen) = sizeof(reply);
+                return IPMI_CC_OK;
+            }
+        }
+
+        else if((*dataLen) == sizeof(setrequest))
+        {
+            std::memcpy(&setrequest, &reqBuf[0], sizeof(setrequest));
+            if(setrequest.op == 1 && boot_check == 1)
+            {
+                reply.subcommand = 0x00;
+                reply.count1 = setrequest.set_byte1;
+                reply.count2 = setrequest.set_byte2;
+                reply.count3 = setrequest.set_byte3;
+                reply.count4 = setrequest.set_byte4;
+                boot_count = reply.count1 + + (reply.count2 << 8) + (reply.count3 << 16)
+                    + (reply.count4 << 24);
+                writeBin(&boot_count,0,sizeof(boot_count));
+                std::memcpy(&replyBuf[0], &reply, sizeof(reply));
+                (*dataLen) = sizeof(reply);
+                return IPMI_CC_OK;
+            } 
+            else if(setrequest.op == 1 && boot_check == 255)
+            {
+                boot_set = 0x01;
+                reply.subcommand = 0x00;
+                reply.count1 = setrequest.set_byte1;
+                reply.count2 = setrequest.set_byte2;
+                reply.count3 = setrequest.set_byte3;
+                reply.count4 = setrequest.set_byte4;
+                boot_count = reply.count1 + + (reply.count2 << 8) + (reply.count3 << 16)
+                    + (reply.count4 << 24);
+                writeBin(&boot_count,0,sizeof(boot_count));
+                writeBin(&boot_set,4,sizeof(boot_set));
+                std::memcpy(&replyBuf[0], &reply, sizeof(reply));
+                (*dataLen) = sizeof(reply);
+                return IPMI_CC_OK;
+            }
+        }
+        else
+            return IPMI_CC_REQ_DATA_LEN_INVALID;
+    }
+
+    ipmi_ret_t handlesyscommand(ipmi_cmd_t, const uint8_t* reqBuf, uint8_t* replyBuf,
+            size_t* dataLen)
+    {
+        if ((*dataLen) < 1)
+        {
+            std::fprintf(stderr, "*dataLen too small: %u\n",
+                    static_cast<uint32_t>(*dataLen));
+            return IPMI_CC_REQ_DATA_LEN_INVALID;
+        }
+        if (reqBuf[0] == 0)
+        {
+            return FiiOemCount(reqBuf, replyBuf, dataLen);
+        }
+    }
+
+    void setupfoxconnOemSysCommands() __attribute__((constructor));    
+    void setupfoxconnOemSysCommands()
+    {
+        static Handler handlerImpl;
+        oem::Router* oemRouter = oem::mutableRouter();
+        std::fprintf(stderr,
+                    "Registering OEM:[%#08X], Cmd:[%#04X] for Sys Commands\n",
+                    oem::foxOemNumber, sysCmd);
+        using namespace std::placeholders;
+        oemRouter->registerHandler(
+            oem::foxOemNumber, sysCmd,
+            handlesyscommand);       
+    }
+
+/////////////////////////////////////////////////////////////////////////////
+/* End of exteded IPMI OEM command which uses the oem, IANA number format */
+/////////////////////////////////////////////////////////////////////////////
+
 }
+
