7a1f94659b68f1a4bc12a5f7dd7c8896eb768122
Willy Tu <wltu@google.com>
2022-11-14 09:27:39 -0800

    add clang-format
    Change-Id: I787041507d3ff6afc6b4a3af3930e8d8363c9570
    Signed-off-by: Willy Tu <wltu@google.com>

--- src/file_handling.cpp
+++ src/file_handling.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
 #include <fcntl.h>
 #include <unistd.h>
+
+#include <iostream>
 
 using namespace std::string_literals;
 std::system_error errnoException(const std::string& message)
@@ -31,17 +32,17 @@ void lseeker(int fd_, size_t offset)
     }
 }
 
-void readBin(int fd_, size_t offset, void *ptr, size_t size)
+void readBin(int fd_, size_t offset, void* ptr, size_t size)
 {
     lseeker(fd_, offset);
     size_t ret = read(fd_, ptr, size);
-    if(ret < 0 )
+    if (ret < 0)
     {
         throw errnoException("Error reading from file"s);
     }
 }
 
-void writeBin(int fd_, size_t offset, void *ptr, size_t size)
+void writeBin(int fd_, size_t offset, void* ptr, size_t size)
 {
     lseeker(fd_, offset);
     ssize_t ret;
@@ -53,7 +54,7 @@ void writeBin(int fd_, size_t offset, void *ptr, size_t size)
     if (static_cast<size_t>(ret) != size)
     {
         throw std::runtime_error(
-                "Tried to send data size "s + std::to_string(size) +
-                " but could only send "s + std::to_string(ret));
+            "Tried to send data size "s + std::to_string(size) +
+            " but could only send "s + std::to_string(ret));
     }
 }
--- src/systemcommands.cpp
+++ src/systemcommands.cpp
@@ -1,3 +1,4 @@
+// clang-format off
 /********************************************************************************
 *                       HON HAI Precision IND.Co., LTD.                         *
 *            Personal Computer & Enterprise Product Business Group              *
@@ -13,59 +14,66 @@
 *     permission of FOXCONN/CESBG/CABG/SRD.                                     *
 *                                                                               *
 ********************************************************************************/
+// clang-format on
 
-
-#include <systemcommands.hpp>
 #include <ipmid/api.hpp>
 #include <phosphor-logging/log.hpp>
 #include <sdbusplus/message/types.hpp>
+#include <systemcommands.hpp>
 
 namespace ipmi
+{
+static void registerSystemFunctions() __attribute__((constructor));
+
+ipmi::RspType<std::vector<uint8_t>>
+    FiiSysPCIeInfo(boost::asio::yield_context yield)
+{
+    std::vector<uint8_t> rsp;
+    char buffer[128], *token;
+    uint32_t value;
+    FILE* pipe = popen(PCIEINFO_COMMAND, "r");
+
+    // Read pcie bifurcation information
+    // it return two bytes, 1st byte bifurcation, 2nd byte present pin
+    if (!pipe)
+        throw std::runtime_error("popen() failed !!!");
+    while (fgets(buffer, sizeof(buffer), pipe) != NULL)
     {
-    static void registerSystemFunctions() __attribute__((constructor));
+        std::cerr << " Command : " << buffer << std::endl;
+    }
+    pclose(pipe);
 
-    ipmi::RspType<std::vector<uint8_t>> FiiSysPCIeInfo(boost::asio::yield_context yield)
+    token = std::strtok(buffer, " ");
+    if (token == NULL)
     {
-        std::vector<uint8_t> rsp;
-        char buffer[128], *token;
-        uint32_t value;
-        FILE *pipe = popen(PCIEINFO_COMMAND, "r");
-
-        // Read pcie bifurcation information
-        // it return two bytes, 1st byte bifurcation, 2nd byte present pin
-        if (!pipe) throw std::runtime_error("popen() failed !!!");
-        while (fgets(buffer, sizeof(buffer), pipe) != NULL)
-        {
-            std::cerr << " Command : " << buffer << std::endl;
-        }
-        pclose(pipe);
-
-        token = std::strtok(buffer, " ");
-        if (token == NULL)
-        {
-            phosphor::logging::log<phosphor::logging::level::ERR>(
-                "Fii system cmd : Error geting PCIe Info came back null");
-            ipmi::responseUnspecifiedError();
-        }
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Fii system cmd : Error geting PCIe Info came back null");
+        ipmi::responseUnspecifiedError();
+    }
+    token = std::strtok(NULL, " ");
+    while (token != NULL)
+    {
+        // std::cerr << " Command token: " << token << std::endl;
+        value = std::stoul(token, nullptr, 16);
+        // std::cerr << " Command value: " << value << ":" << std::hex << value
+        // << std::endl;
+        rsp.push_back(static_cast<uint8_t>(value & 0xFF));
         token = std::strtok(NULL, " ");
-        while (token != NULL)
-        {
-            //std::cerr << " Command token: " << token << std::endl;
-            value = std::stoul(token, nullptr, 16);
-            //std::cerr << " Command value: " << value << ":" << std::hex << value << std::endl;
-            rsp.push_back(static_cast<uint8_t>(value & 0xFF));
-            token = std::strtok(NULL, " ");
-        }
-
-        return ipmi::responseSuccess(rsp);
     }
 
-    void registerSystemFunctions()
-    {
-        std::fprintf(stderr, "Registering OEM:[0x34], Cmd:[%#04X] for Fii System OEM Commands\n", FII_CMD_SYS_PCIE_INFO);
-        ipmi::registerHandler(ipmi::prioOemBase, ipmi::netFnOemThree, FII_CMD_SYS_PCIE_INFO, ipmi::Privilege::User,
-                FiiSysPCIeInfo);
-
-        return;
-    }
+    return ipmi::responseSuccess(rsp);
 }
+
+void registerSystemFunctions()
+{
+    std::fprintf(
+        stderr,
+        "Registering OEM:[0x34], Cmd:[%#04X] for Fii System OEM Commands\n",
+        FII_CMD_SYS_PCIE_INFO);
+    ipmi::registerHandler(ipmi::prioOemBase, ipmi::netFnOemThree,
+                          FII_CMD_SYS_PCIE_INFO, ipmi::Privilege::User,
+                          FiiSysPCIeInfo);
+
+    return;
+}
+} // namespace ipmi
