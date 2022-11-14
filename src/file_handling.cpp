ff2bf5e8f8e570028d5dccabe9a608bc4714a6b7
Willy Tu <wltu@google.com>
2022-11-14 08:36:56 -0800

    fix read output type to use ssize_t
    size_t is never negative
    ```
    size_t ret = read(fd_, ptr, size);
    if (ret < 0)
      // This will never get here.
    }
    ```
    
    Change-Id: I5d920537908f8f9a463f1deb77bbdfb06fc830bb
    Signed-off-by: Willy Tu <wltu@google.com>

--- src/file_handling.cpp
+++ src/file_handling.cpp
@@ -3,12 +3,12 @@
 #include <unistd.h>
 
 using namespace std::string_literals;
-std::system_error errnoException(const std::string& message)
+std::system_error errnoException(const std::string &message)
 {
     return std::system_error(errno, std::generic_category(), message);
 }
 
-int sysopen(const std::string& path)
+int sysopen(const std::string &path)
 {
     int fd_ = open(path.c_str(), O_RDWR);
     if (fd_ < 0)
@@ -34,8 +34,8 @@ void lseeker(int fd_, size_t offset)
 void readBin(int fd_, size_t offset, void *ptr, size_t size)
 {
     lseeker(fd_, offset);
-    size_t ret = read(fd_, ptr, size);
-    if(ret < 0 )
+    ssize_t ret = read(fd_, ptr, size);
+    if (ret < 0)
     {
         throw errnoException("Error reading from file"s);
     }
@@ -53,7 +53,7 @@ void writeBin(int fd_, size_t offset, void *ptr, size_t size)
     if (static_cast<size_t>(ret) != size)
     {
         throw std::runtime_error(
-                "Tried to send data size "s + std::to_string(size) +
-                " but could only send "s + std::to_string(ret));
+            "Tried to send data size "s + std::to_string(size) +
+            " but could only send "s + std::to_string(ret));
     }
 }
